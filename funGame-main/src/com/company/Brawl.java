8604b05d2a195948e48fa2ed4a03512419fa8bbd
Joco321 <32468492+Joco321@users.noreply.github.com>
2022-07-07 22:25:11 -0600

    hello

--- funGame-main/src/com/company/Brawl.java
+++ funGame-main/src/com/company/Brawl.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Brawl {
     public static boolean fight(Monster monster, Character soldier){
 
-        //----Setting up all the stats for the character----iiiii---
+        //----Setting up all the stats for the character----hello---
         Item hold = soldier.getEquippedItem();
         double attackStats = currentStats(hold, soldier);
         double defenseStats = currentDStats(hold, soldier);
